cab3e37f76008bd7d755e3cee3464f35dd737e72
Daniel Duarte <daniel.skapunk@gmail.com>
2017-11-17 04:22:20 -0600

    ignore dist y node_modules

---
+++ README.md
@@ -0,0 +1,12 @@
+# dConsole
+Console log wrapper with line numbers
+
+forked from gist: [amelierosser gist](https://gist.github.com/amelierosser/e1d36b8685b5f81508d0aa65dc15a36d)
+
+## Usage:
+```javascript
+ import * as c from 'console';
+ c.log('abc');
+ ```
+
+ * Ref: https://matthewspencer.github.io/console-log/
---
+++ src/console.js
@@ -0,0 +1,62 @@
+/**
+ * console wrapper with line numbers
+ * forked from: amelierosser/console.js
+ * Usage:
+ * import * as c from 'console'
+ * c.log('abc');
+ *
+ * Ref: https://matthewspencer.github.io/console-log/
+ */
+
+/*eslint-disable */
+const slice = [].slice;
+const enabled = window.location.hostname.indexOf('localhost') !== -1;
+
+export const log = function log() {
+	if (!window.console || !console.log) {
+		return function() {};
+	}
+	if (!enabled) return function() {};
+	return Function.prototype.bind.call(console.log, console);
+}();
+
+export const clear = function clear() {
+	if (!window.console || !console.clear) {
+		return function() {};
+	}
+	if (!enabled) return function() {};
+	return Function.prototype.bind.call(console.clear, console);
+}();
+
+export const debug = function debug() {
+	if (!window.console || !console.debug) {
+		return function() {};
+	}
+	if (!enabled) return function() {};
+	return Function.prototype.bind.call(console.debug, console);
+}();
+
+export const info = function info() {
+	if (!window.console || !console.info) {
+		return function() {};
+	}
+	if (!enabled) return function() {};
+	return Function.prototype.bind.call(console.info, console);
+}();
+
+export const warn = function warn() {
+	if (!window.console || !console.warn) {
+		return function() {};
+	}
+	if (!enabled) return function() {};
+	return Function.prototype.bind.call(console.warn, console);
+}();
+
+export const error = function error() {
+	if (!window.console || !console.error) {
+		return function() {};
+	}
+	if (!enabled) return function() {};
+	return Function.prototype.bind.call(console.error, console);
+}();
+/*eslint-enable */
